9a482c0d01e9ab3d146fa106be2079cef1bbfd28
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-02-11 20:40:03 +0000

    guesser code ish

---
+++ webapp/foodguesser/views/guesser.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+def guess(request):
+    if request.method == "POST":
+        guessid = request.POST.get("id")
+        guesstimate = request.POST.get("guess")
+
+        if(guesstimate and guessid):
+            if(Food.objects.get(id=guessid).calories = guesstimate):
+                session_add_score(request)
+            
+            return HttpRedirect(reverse("guess"))
+        else:
+            return HttpResponse("Didnt do a guesstimate")
+            
+    image = null
+    return render(request, "guesser/guess.html", {"img": image})
+
+
+
+
+
+#Helper functions
+def session_add_score(request):
+    score = request.session.get("score")
+    
+    if(score):
+        score += 1
+    else:
+        score = 1
+
+    request.sesion["score"] = score
+        
