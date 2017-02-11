83f4d095d8df05b562c127c8cb3fc22dab05c23b
domantasjurkus <domantas.jurkus@gmail.com>
2017-02-11 21:26:30 +0000

    Added base frontend

--- webapp/foodguesser/views/base.py
+++ webapp/foodguesser/views/base.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "base/index.html", {})
+    return render(request, "foodguesser/base/index.html", {})
 
 def about(request):
     return render(request, "base/about.html", {})
---
+++ webapp/static/js/script.js
@@ -0,0 +1,48 @@
+jQuery(function(){
+
+    $('input[name="cabin"]').change(function() {
+        $('.crystal').toggleClass('fade');
+        $('.standard').toggleClass('fade');
+    });
+
+    /*$('.expand').click(function(){
+        var text = $(this).text();
+        $(this).parent().next().slideToggle("fast");
+        $(this).text(text == "More Details" ? "Hide Details" : "More Details");
+    });*/
+
+    // On submit of checkbox form
+    $(".btn-checkbox-submit").click(function(e) {
+        e.preventDefault();
+        console.log($(this));
+
+        $('.nav-tabs a[href="#packages"]').tab('show');
+    });
+
+
+    $(".btn-01").click(function () {
+        $('.nav-tabs a[href="#question02"]').tab('show');
+    });
+
+    $(".btn-02").click(function () {
+        $('.nav-tabs a[href="#question03"]').tab('show');
+    });
+
+    $(".btn-03").click(function () {
+        $('.nav-tabs a[href="#packages"]').tab('show');
+    });
+
+    // Go to heatmap
+    $(".availability").click(function () {
+        $('.nav-tabs a[href="#heatmap"]').tab('show');
+    });
+
+    $(".highcharts-point").click(function () {
+        $('.nav-tabs a[href="#payment"]').tab('show');
+    });
+
+    $(".btn-book").click(function () {
+        $('.nav-tabs a[href="#done"]').tab('show');
+    });
+
+});
