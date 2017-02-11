1d5d87c6b9783372475050f3a5583ee4fd197278
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-02-11 19:28:54 +0000

    Fixed borkedness

--- webapp/foodguesser/apps.py
+++ webapp/foodguesser/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class DebateConfig(AppConfig):
+class FoodguesserConfig(AppConfig):
     name = 'foodguesser'
--- webapp/foodguesser/models.py
+++ webapp/foodguesser/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-class ChatRoom(models.Model):
-    topic = models.CharField(max_length=200)
-
-
-    def __str__(self):
-        return this.topic
+# Create your models here.
